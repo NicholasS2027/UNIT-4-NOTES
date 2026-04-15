ac51769f3e2bd4a6084de4c183f96ec518a0b2c5
Nick <nicholas.strazicic2027@gbwl.org>
2026-04-15 14:43:07 +0000

    variables

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hello world");
+
+// Create a variable that holds a number
+let luckyNum = 13; //numbers dont need quotes!
+console.log(luckyNum);
+// CHANGE the value stored in a variable (dont repeat LET)
+luckyNum = 7;
+console.log(luckyNum);
