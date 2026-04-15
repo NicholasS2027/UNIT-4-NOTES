29170ff0622b5376049eced5b7983aaaf3b2142d
Nick <nicholas.strazicic2027@gbwl.org>
2026-04-15 15:18:37 +0000

    operators

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ console.log(luckyNum);
 // CHANGE the value stored in a variable (dont repeat LET)
 luckyNum = 7;
 console.log(luckyNum);
+
+// MATH OPERATORS (+, -, *, /)
+let age = 16;
+console.log(age);
+let ageInTen = age + 10;
+console.log(ageInTen);
+let ageDogYears = age / 7;
+console.log(ageDogYears);
+// can also do math with just numbers
+let plutoHumanAge = 12 * 7;
+console.log(plutoHumanAge);
+
+// Combine 2 "literal" strings (CONCATENATION)
+console.log("hello " + " world");
+// OR concatenate a literal string with a variable
+console.log("My age is " + age);
+console.log("My age in ten years is " + ageInTen);
+console.log("My age in dog years is " + ageDogYears);
+
