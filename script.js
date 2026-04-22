9ee867819a6ad0b5bed3ea82d1e25ab358a977b4
Nick <nicholas.strazicic2027@gbwl.org>
2026-04-22 15:13:42 +0000

    input -> output changes

--- script.js
+++ script.js
@@ -78,3 +78,20 @@ console.log(message)
     }
     // CALL the function to use it!
     skincare();
+
+    // ANALOGY: Functions are like RECIPES
+    // Ex. of a function that needs PARAMETERS (input/"ingredients") and returns a new value (output/"meal")
+
+    // if you need parameters, list some variables inside the parenthesis after the function name
+    function makeSando(filling, sauce, bread) {
+        let sando; // new variable to represent sandwich
+        sando = filling + " and " + sauce + " on " + bread;
+        return sando; // OUTPUT!!
+    } 
+
+    // must "pass in" actual arguments to use the function
+    let mySando = makeSando("american", "vodka", "white bread");
+    console.log(mySando);
+
+    // shortcut to see return values
+    console.log( makeSando("pb", "j", "white bread") );
